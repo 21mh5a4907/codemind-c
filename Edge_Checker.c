a42b32634b6f1c0598f3833285b881149574b416
Vinay yadavilli <109718446+21mh5a4907@users.noreply.github.com>
2022-08-30 01:31:01 -0800

    #include<iostream> using namespace std; int main() {     int a,b;     cin>>a>>b;     if(a-b==1 || a-b==-1 || b-a==9 || b-a==-9)     {         cout<<"Yes";     }     else{         cout<<"No";     } }

---
+++ Edge_Checker.c
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    if(a-b==1 || a-b==-1 || b-a==9 || b-a==-9)
+    {
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+}
